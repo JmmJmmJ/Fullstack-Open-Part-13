9aa933da5badc786b7b66414ef0b52dcb18f390f
JmmJmmJ <jy.maki@gmail.com>
2024-09-09 11:01:15 +0300

    13.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -11,50 +12,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false
     }
   },
-})
-
-
-class Note extends Model {}
-
-
-Note.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  content: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  important: {
-    type: DataTypes.BOOLEAN
-  },
-  date: {
-    type: DataTypes.DATE
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'note'
-})
-
-app.get('/api/notes', async (req, res) => {
-
-  const notes = await Note.findAll()
-  res.json(notes)
-})
-
-app.post('/api/notes', async (req, res) => {
-  console.log(req.body)
-  const note = await Note.create(req.body)
-  res.json(note)
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
 })
 
 class Blog extends Model {}
@@ -86,16 +43,34 @@ Blog.init({
   timestamps: false,
   modelName: 'blog'
 })
+Blog.sync()
 
-const blogit = async () => {
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
   try {
-    const blogs = await Blog.findAll()
-      blogs.forEach((blog) => {
-        console.log(blog.dataValues.author + ": " + "'" + blog.dataValues.title+ "'" + ", " + blog.dataValues.likes + " likes")
-      })
-  } catch (error) {
-    console.log(error)
+    console.log(req.body)
+  const blog = await Blog.create(req.body)
+  return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({error})
   }
-}
+})
 
-blogit()
+app.delete('/api/blogs/:id', async (req, res) => {
+  const id = req.params.id
+  await Blog.destroy({
+    where: {
+      id: id
+    }
+  })
+  res.status(200).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
