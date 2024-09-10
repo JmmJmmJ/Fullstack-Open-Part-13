84ac429caaa0c0cf85900f177645c8b06e2884bf
JmmJmmJ <jy.maki@gmail.com>
2024-09-10 09:06:43 +0300

    "13.8"

---
+++ controllers/users.js
@@ -0,0 +1,40 @@
+const router = require('express').Router()
+
+const models = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await models.User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await models.User.create(req.body)
+    res.json(user)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.put('/:username', async (req, res, next) => {
+    try {
+        const user = await models.User.findOne({
+            where: {
+                username: req.params.username
+            },
+          });
+
+          if (user) {
+            user.username = req.body.username
+            await user.save()
+            res.json(user)
+        } else {
+            res.status(404).end
+        }
+    } catch(error) {
+        next(error)
+    }
+
+  })
+
+module.exports = router
