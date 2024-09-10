ead0cecf9b4db037fac96a07addee206a2d46ce2
JmmJmmJ <jy.maki@gmail.com>
2024-09-10 11:51:38 +0300

    "13.11"

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,47 +1,91 @@
 const router = require('express').Router()
-
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 const models = require('../models')
 
-router.get('/', async (req, res) => {
-    const blogs = await models.Blog.findAll()
-    res.json(blogs)
-  })
-  
-  router.post('/', async (req, res, next) => {
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
     try {
-      console.log(req.body)
-    const blog = await models.Blog.create(req.body)
-    return res.json(blog)
-    } catch(error) {
-      next(error)
+      console.log(authorization.substring(7))
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
     }
-  })
-  
-  router.delete('/:id', async (req, res) => {
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await models.Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', tokenExtractor, async (req, res, next) => {
+  try {
+    const user = await models.User.findByPk(req.decodedToken.id)
+    const blog = await models.Blog.create({ ...req.body, userId: user.id })
+    return res.json(blog)
+  } catch (error) {
+    next(error)
+  }
+})
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, SECRET);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+
     const id = req.params.id
-    await models.Blog.destroy({
+    const blog = await models.Blog.findOne({
       where: {
         id: id
-      }
-    })
-    res.status(200).end()
-  })
+      },
+    });
 
-  router.put('/:id', async (req, res, next) => {
-    try {
+
+    if (blog.userId == decodedToken.id) {
+      await models.Blog.destroy({
+        where: {
+          id: id
+        }
+      })
+    }
+  } catch(error) {
+    next(error)
+  }
+  res.status(200).end()
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
     blog = await models.Blog.findByPk(req.params.id)
 
     if (blog) {
-        blog.likes = req.body.likes
-        await blog.save()
-        res.json(blog)
+      blog.likes = req.body.likes
+      await blog.save()
+      res.json(blog)
     } else {
-        res.status(404).end
+      res.status(404).end
     }
-    } catch(error) {
-        next(error)
-    }
+  } catch (error) {
+    next(error)
+  }
 
-  })
+})
 
-  module.exports = router
+module.exports = router
