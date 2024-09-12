d9f8c5564454cbb26723d66adc3b8cc0f4a02515
JmmJmmJ <jy.maki@gmail.com>
2024-09-12 16:49:30 +0300

    13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,14 +1,45 @@
 const router = require('express').Router()
-
+const { SECRET } = require('../util/config')
 const models = require('../models')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 router.post('/', async (req, res, next) => {
-    try {
+  try {
     const list = await models.Readinglist.create({ ...req.body, userId: req.body.user_id, blogId: req.body.blog_id })
-      res.json(list)
-    } catch(error) {
-      next(error)
+    res.json(list)
+  } catch (error) {
+    next(error)
   }
-  })
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, SECRET);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+
+    lista = await models.Readinglist.findByPk(req.params.id)
+    if (lista.userId == decodedToken.id) {
+      lista.read = req.body.read
+      await lista.save()
+      res.json(lista)
+    } else {
+      return response.status(400).json({ error: "can't find" });
+    }
+
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
