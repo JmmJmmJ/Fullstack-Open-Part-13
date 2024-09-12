71e26142c7fe80077baecfdf32e8438b1f75be94
JmmJmmJ <jy.maki@gmail.com>
2024-09-12 17:32:52 +0300

    13.23

--- controllers/users.js
+++ controllers/users.js
@@ -8,62 +8,82 @@ router.get('/', async (req, res) => {
       model: models.Blog,
       as: 'reading',
     },
-  ]
+    ]
   })
   res.json(users)
 })
 
 router.get('/:id', async (req, res, next) => {
   try {
-  const users = await models.User.findOne({
-    attributes: ['username', 'name'],
-    where: {
-      id: req.params.id,
-    },
-    include: {
-      model: models.Blog,
-      as: 'reading',
-      attributes: ['id', 'author', 'url', 'title', 'likes', 'year'],
-      through: {
-        attributes: ['id','read' ]
-      }
-    }, 
-  })
-  console.log(users.reading)
-  res.json(users)
-} catch(error) {
-  next(error)
-}
+    const read = req.query.read
+
+    if (!read) {
+      const users = await models.User.findOne({
+        attributes: ['username', 'name'],
+        where: {
+          id: req.params.id,
+        },
+        include: {
+          model: models.Blog,
+          as: 'reading',
+          attributes: ['id', 'author', 'url', 'title', 'likes', 'year'],
+          through: {
+            attributes: ['id', 'read']
+          }
+        },
+      })
+      res.json(users)
+    } else {
+      const users = await models.User.findOne({
+        attributes: ['username', 'name'],
+        where: {
+          id: req.params.id,
+        },
+        include: {
+          model: models.Blog,
+          as: 'reading',
+          attributes: ['id', 'author', 'url', 'title', 'likes', 'year'],
+          through: {
+            attributes: ['id', 'read'],
+            where: { read: read }
+          }
+        },
+      })
+      res.json(users)
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
   try {
     const user = await models.User.create(req.body)
     res.json(user)
-  } catch(error) {
+  } catch (error) {
     next(error)
-}
+  }
 })
 
 router.put('/:username', async (req, res, next) => {
-    try {
-        const user = await models.User.findOne({
-            where: {
-                username: req.params.username
-            },
-          });
+  try {
+    const user = await models.User.findOne({
+      where: {
+        username: req.params.username
+      },
+    });
 
-          if (user) {
-            user.username = req.body.username
-            await user.save()
-            res.json(user)
-        } else {
-            res.status(404).end
-        }
-    } catch(error) {
-        next(error)
+    if (user) {
+      user.username = req.body.username
+      await user.save()
+      res.json(user)
+    } else {
+      res.status(404).end
     }
+  } catch (error) {
+    next(error)
+  }
 
-  })
+})
 
 module.exports = router
